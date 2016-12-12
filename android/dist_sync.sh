e2ec7e0122f92842648889baf53150c15c3bad87
stiartsly <stiartsly@gmail.com>
2016-12-12 09:46:59 +0800

    update dist_sync

--- android/dist_sync.sh
+++ android/dist_sync.sh
@@ -2,37 +2,47 @@
 
 DST_TOP_DIR=../../WhisperManagedCore-Android
 SRC_DIR=./_dist
-DST_DIR=${DST_TOP_DIR}/app/distribution/libs
+DST_DIR=${DST_TOP_DIR}/app/distribution
 
 if [ ! -d "${DST_TOP_DIR}" ]; then
     echo "Error, script should run in wicore5.0/build directory"
     exit 1
 fi
 
+if [ x = y ]; then
 CMD="./android_build.sh whisper"
 for m in arm arm64 x86 x86_64 mips; do
     $CMD clean
     $CMD
 done
+fi
 
-update_libwhisper() {
-    echo "coping libwhisper.a to $2"
+update_library() {
+    echo "Copying library libwhisper.a to $2"
     cp -f $1/libwhisper.a $2/.
-    cd $2 && ls -la libwhisper.a && cd - 
     echo ""
 }
 
+update_headers() {
+    for h in whisper.h whisper_ext.h whisper_portforwarding.h; do
+        echo "Copying header $h to $2"
+        cp -f $1/$h $2/.
+    done
+    echo ""
+}
+
+update_headers ${SRC_DIR}/Android-arm/debug/include ${DST_DIR}/include
 for arch in arm arm64 x86 x86_64; do
     case $arch in
         arm)
-            update_libwhisper ${SRC_DIR}/Android-${arch}/lib ${DST_DIR}/armeabi
-            update_libwhisper ${SRC_DIR}/Android-${arch}/lib ${DST_DIR}/armeabi-v7a
+            update_library ${SRC_DIR}/Android-${arch}/debug/lib ${DST_DIR}/libs/armeabi
+            update_library ${SRC_DIR}/Android-${arch}/debug/lib ${DST_DIR}/libs/armeabi-v7a
             ;;
         arm64)
-            update_libwhisper ${SRC_DIR}/Android-${arch}/lib ${DST_DIR}/arm64-v8a
+            update_library ${SRC_DIR}/Android-${arch}/debug/lib ${DST_DIR}/libs/arm64-v8a
             ;;
         x86 | x86_64)
-            update_libwhisper ${SRC_DIR}/Android-${arch}/lib ${DST_DIR}/${arch}
+            update_library ${SRC_DIR}/Android-${arch}/debug/lib ${DST_DIR}/libs/${arch}
             ;;
     esac
 done
