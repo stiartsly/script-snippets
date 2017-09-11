ef9e278c96a2093f5de7fd28166cfb02dd1f9670
stiartsly <stiartsly@gmail.com>
2017-09-11 10:02:25 +0800

    update

---
+++ wmpfd/wmpfdc.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+HOST=$(uname -s)
+ARCH=$(uname -m)
+
+case ${HOST} in
+    "Linux" | "Darwin")
+        ;;
+    *)
+        echo "Error: $0 should run on Darwin/Linux"
+        exit 1;;
+esac
+
+DSO_PATH="${PWD}/../../build/_dist/${HOST}-${ARCH}/debug/lib"
+
+if [ ! -d ${DSO_PATH} ]; then
+     echo "Error: whisper library path ${DSO_PATH} not exist"
+     exit 1
+fi
+
+case ${HOST} in
+    "Linux")
+        LD_LIBRARY_PATH="${DSO_PATH}" ./wmpfd --deviceid=wmpfdclient -c client.conf $*
+        ;;
+    "Darwin")
+        DYLD_LIBRARY_PATH="${DSO_PATH}" ./wmpfd --device=wmpfdclient -c client.conf $*
+        ;;
+esac
+
+exit 0
+
